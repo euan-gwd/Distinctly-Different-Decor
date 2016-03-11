801b67e3cbf657924b033780a009a7b81e679453
Euan_G <euan@amwic.co.za>
2016-03-11 13:26:22 +0200

    Added AngularJS Controllers and ng-Repeat

--- build/js/app.js
+++ build/js/app.js
@@ -1,3 +1,11 @@
 (function() {
-    var app = angular.module('DDDApp', ['ngRoute']);
+    var app = angular.module('DDDApp', ['ui.router']);
+
+    app.controller('MainCtrl', ['$scope', function($scope) {
+        $scope.list = [
+            { name: 'one', desc: 'this is the 1st box in the store', price: '1' },
+            { name: 'two', desc: 'this is the 2nd box in the store', price: '2' },
+            { name: 'three', desc: 'this is the 3rd box in the store', price: '3' }
+        ];
+    }]);
 }());
