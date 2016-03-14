35f96994171ddb1c87698930d5ac3373742d2803
Euan_G <euan@amwic.co.za>
2016-03-14 14:30:17 +0200

    Added ng-repeat

--- build/js/app.js
+++ build/js/app.js
@@ -3,21 +3,40 @@
     var app = angular.module('DDDApp', ['ui.router']);
 
     app.controller('StoreController', ['', function() {
-    	this.product = storeItems;
+        this.products = storeInventory;
 
-        storeList.counter = 0;
-        storeList.countadd = function(inc) {
-            storeList.counter += inc;
-        };
-        storeList.countremove = function(inc) {
-            storeList.counter -= inc;
-        };
+        // storeList.counter = 0;
+        // storeList.countadd = function(inc) {
+        //     storeList.counter += inc;
+        // };
+        // storeList.countremove = function(inc) {
+        //     storeList.counter -= inc;
+        // };
 
     }]);
-    var storeItems = [
-            { name: 'one', desc: 'this is the 1st box in the store', price: '1' },
-            { name: 'two', desc: 'this is the 2nd box in the store', price: '2' },
-            { name: 'three', desc: 'this is the 3rd box in the store', price: '3' },
-            { name: 'four', desc: 'this is the 4th box in the store', price: '4' }
-        ];
+    var storeInventory = [{
+        name: 'one',
+        desc: 'this is the 1st box in the store',
+        price: '1',
+        canPurchase: true,
+        soldOut: false
+    }, {
+        name: 'two',
+        desc: 'this is the 2nd box in the store',
+        price: '2',
+        canPurchase: true,
+        soldOut: false
+    }, {
+        name: 'three',
+        desc: 'this is the 3rd box in the store',
+        price: '3',
+        canPurchase: true,
+        soldOut: false
+    }, {
+        name: 'four',
+        desc: 'this is the 4th box in the store',
+        price: '4',
+        canPurchase: true,
+        soldOut: false
+    }];
 }());
