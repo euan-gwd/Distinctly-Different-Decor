017b910cb25932cb545ffcc9aa9fac0271bfa372
Euan_G <euan@amwic.co.za>
2016-03-14 15:59:17 +0200

    fixed color

--- build/js/app.js
+++ build/js/app.js
@@ -19,28 +19,24 @@
         desc: 'this is the 1st box in the store',
         image: 'http://placehold.it/330x150',
         price: '1',
-        canPurchase: true,
         soldOut: false
     }, {
         name: 'two',
         desc: 'this is the 2nd box in the store',
         price: '2',
         image: 'http://placehold.it/330x150',
-        canPurchase: true,
         soldOut: false
     }, {
         name: 'three',
         desc: 'this is the 3rd box in the store',
         price: '3',
         image: 'http://placehold.it/330x150',
-        canPurchase: true,
         soldOut: false
     }, {
         name: 'four',
         desc: 'this is the 4th box in the store',
         price: '4',
         image: 'http://placehold.it/330x150',
-        canPurchase: true,
         soldOut: false
     }];
 }());
