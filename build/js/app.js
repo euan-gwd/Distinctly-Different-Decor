9e7d05d4bce101af62349a9086394641af5b95fc
Euan_G <euan@amwic.co.za>
2016-03-14 13:28:49 +0200

    initial feature commit

--- build/js/app.js
+++ build/js/app.js
@@ -1,21 +1,23 @@
 (function() {
+    "use strict";
     var app = angular.module('DDDApp', ['ui.router']);
 
-    app.controller('MainCtrl', ['$scope', function($scope) {
-        $scope.list = [
+    app.controller('StoreController', ['', function() {
+    	this.product = storeItems;
+
+        storeList.counter = 0;
+        storeList.countadd = function(inc) {
+            storeList.counter += inc;
+        };
+        storeList.countremove = function(inc) {
+            storeList.counter -= inc;
+        };
+
+    }]);
+    var storeItems = [
             { name: 'one', desc: 'this is the 1st box in the store', price: '1' },
             { name: 'two', desc: 'this is the 2nd box in the store', price: '2' },
             { name: 'three', desc: 'this is the 3rd box in the store', price: '3' },
             { name: 'four', desc: 'this is the 4th box in the store', price: '4' }
         ];
-
-        $scope.counter = 0;
-        $scope.countadd = function(inc) {
-        	$scope.counter += inc;
-        };
-        $scope.countremove = function(inc) {
-        	$scope.counter -= inc;
-        };
-
-    }]);
 }());
