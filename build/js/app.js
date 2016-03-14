22f13865b14d6c40e1f3f44ff61feaa929e82935
Euan_G <euan@amwic.co.za>
2016-03-14 15:22:44 +0200

    Restricted code to fit best practices

--- build/js/app.js
+++ build/js/app.js
@@ -2,21 +2,21 @@
     "use strict";
     var app = angular.module('DDDApp', ['ui.router']);
 
-    app.controller('StoreController', ['', function() {
+    app.controller('StoreController', function() {
         this.products = storeInventory;
+		
+		// Updates the counter badge on the shopping cart  
+        this.counter = 0;
+        this.countAdd = function(inc) {
+            this.counter += inc;
+        };
+    
+    });
 
-        // storeList.counter = 0;
-        // storeList.countadd = function(inc) {
-        //     storeList.counter += inc;
-        // };
-        // storeList.countremove = function(inc) {
-        //     storeList.counter -= inc;
-        // };
-
-    }]);
     var storeInventory = [{
         name: 'one',
         desc: 'this is the 1st box in the store',
+        image: 'http://placehold.it/330x150',
         price: '1',
         canPurchase: true,
         soldOut: false
@@ -24,18 +24,21 @@
         name: 'two',
         desc: 'this is the 2nd box in the store',
         price: '2',
+        image: 'http://placehold.it/330x150',
         canPurchase: true,
         soldOut: false
     }, {
         name: 'three',
         desc: 'this is the 3rd box in the store',
         price: '3',
+        image: 'http://placehold.it/330x150',
         canPurchase: true,
         soldOut: false
     }, {
         name: 'four',
         desc: 'this is the 4th box in the store',
         price: '4',
+        image: 'http://placehold.it/330x150',
         canPurchase: true,
         soldOut: false
     }];
