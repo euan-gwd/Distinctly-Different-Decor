618ad2efb3003d710a94f9b2f02d58efad813f5f
Euan_G <euan@amwic.co.za>
2016-03-22 13:30:37 +0200

    Added calculation for total items in cart but has incorrect calculation bug

--- build/js/app.js
+++ build/js/app.js
@@ -4,6 +4,7 @@
 
     app.controller('StoreController', function($scope, $http) {
         $scope.cart = [];
+        var cartCount;
 
         // Load products from server
         $http.get('products.json').success(function(response) {
@@ -22,22 +23,28 @@
             if (!found) {
                 $scope.cart.push(angular.extend({ quantity: 1 }, product));
             }
+            cartCount = $scope.cart.length - 1;
         };
 
         //Remove product from basket
         $scope.removeFromCart = function(product, index) {
+            var found = false;
             $scope.cart.forEach(function(item) {
                 if (item.id === product.id) {
                     item.quantity--;
+                    found = true;
                     if (item.quantity === 0) {
                         $scope.cart.splice(index, 1);
                     }
                 }
             });
-
+            if (!found) {
+                $scope.cart.splice(index, angular.extend({ quantity: -1 }, product), 1);
+            }
+            cartCount = $scope.cart.length;
         };
 
-        // Calculate totals in basket
+        // Calculate total price for products in Cart
         $scope.getCartPrice = function() {
             var total = 0;
             $scope.cart.forEach(function(product) {
@@ -46,5 +53,16 @@
             return total;
         };
 
+        // Calculate total number of items in Cart
+        $scope.getCartTotals = function() {
+            var totalInCart = 0;
+
+            cartCount = $scope.cart.length;
+            $scope.cart.forEach(function(product) {
+                totalInCart += cartCount * product.quantity;
+            });
+
+            return totalInCart;
+        };
     });
 }());
