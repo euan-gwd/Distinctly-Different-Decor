bed45c435ffa5564d0b1ff44045d33f77d0f1f9e
Euan_G <euan@amwic.co.za>
2016-03-22 21:32:59 +0200

    fixed total calculations bug in cart

--- build/js/app.js
+++ build/js/app.js
@@ -3,8 +3,9 @@
     var app = angular.module('DDDApp', []);
 
     app.controller('StoreController', function($scope, $http) {
-        $scope.cart = [];
-        var cartCount;
+        $scope.cart = {
+            products: []
+        };
 
         // Load products from server
         $http.get('products.json').success(function(response) {
@@ -14,40 +15,38 @@
         // Add products to basket
         $scope.addToCart = function(product) {
             var found = false;
-            $scope.cart.forEach(function(item) {
+            $scope.cart.products.forEach(function(item) {
                 if (item.id === product.id) {
                     item.quantity++;
                     found = true;
                 }
             });
             if (!found) {
-                $scope.cart.push(angular.extend({ quantity: 1 }, product));
+                $scope.cart.products.push(angular.extend({ quantity: 1 }, product));
             }
-            cartCount = $scope.cart.length - 1;
         };
 
         //Remove product from basket
         $scope.removeFromCart = function(product, index) {
             var found = false;
-            $scope.cart.forEach(function(item) {
+            $scope.cart.products.forEach(function(item) {
                 if (item.id === product.id) {
                     item.quantity--;
                     found = true;
-                    if (item.quantity === 0) {
-                        $scope.cart.splice(index, 1);
+                    if (item.quantity == 0) {
+                        $scope.cart.products.splice(index, 1);
                     }
                 }
             });
             if (!found) {
-                $scope.cart.splice(index, angular.extend({ quantity: -1 }, product), 1);
+                $scope.cart.products.splice(angular.extend({ quantity: -1 }, product));
             }
-            cartCount = $scope.cart.length;
         };
 
         // Calculate total price for products in Cart
         $scope.getCartPrice = function() {
             var total = 0;
-            $scope.cart.forEach(function(product) {
+            $scope.cart.products.forEach(function(product) {
                 total += product.price * product.quantity;
             });
             return total;
@@ -55,13 +54,14 @@
 
         // Calculate total number of items in Cart
         $scope.getCartTotals = function() {
-            var totalInCart = 0;
+            var cartCount,
+                totalInCart = 0;
 
-            cartCount = $scope.cart.length;
-            $scope.cart.forEach(function(product) {
-                totalInCart += cartCount * product.quantity;
+            cartCount = $scope.cart.products.length;
+            console.log($scope.cart.products);
+            $scope.cart.products.forEach(function(product) {
+                totalInCart = parseInt(totalInCart) + product.quantity;
             });
-
             return totalInCart;
         };
     });
