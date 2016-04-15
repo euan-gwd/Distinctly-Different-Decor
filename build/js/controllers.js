80ea07e7cb86f7f056fe9cd07016b928bec09dcf
Euan_G <euan@amwic.co.za>
2016-04-15 15:45:45 +0200

    Added Form Validation

--- build/js/controllers.js
+++ build/js/controllers.js
@@ -1,112 +1,117 @@
 (function() {
-    'use strict';
-    var app = angular.module('DDDApp.controllers', []);
+  'use strict';
+  var app = angular.module('DDDApp.controllers', []);
 
-    app.controller('StoreController', function($scope, $state, dataService) {
-        $scope.cart = {
-            products: []
-        };
-        // Load products from server
-        dataService.getProducts(function(response) {
-            $scope.products = response.data;
-        });
-
-        // Add products to basket
-        $scope.addToCart = function(product) {
-            var found = false;
-            $scope.cart.products.forEach(function(item) {
-                if (item.id === product.id) {
-                    item.quantity++;
-                    found = true;
-                }
-            });
-            if (!found) {
-                $scope.cart.products.push(angular.extend({ quantity: 1 }, product));
-            }
-        };
-
-        // Calculate total price for products in Cart
-        $scope.getCartPrice = function() {
-            var total = 0;
-            $scope.cart.products.forEach(function(product) {
-                total += product.price * product.quantity;
-            });
-            return total;
-        };
-
-        // Calculate total number of items in Cart
-        $scope.getCartTotals = function() {
-            var totalInCart = 0;
-
-            $scope.cart.products.forEach(function(product) {
-                totalInCart = parseInt(totalInCart) + product.quantity;
-            });
-            return totalInCart;
-        };
-
-        // Cart Checkout
-        $scope.cartCheckOut = function() {
-            var checkout = $scope.cart;
-            window.sessionStorage.checkout = angular.toJson(checkout);
-            $state.go('checkout');
-        };
+  app.controller('StoreController', function($scope, $state, dataService) {
+    $scope.cart = {
+      products: []
+    };
+    // Load products from server
+    dataService.getProducts(function(response) {
+      $scope.products = response.data;
     });
 
-    app.controller('CartController', function($scope, $state) {
-        var cartItems = angular.fromJson(window.sessionStorage.checkout || '[]');
-        $scope.cart = cartItems;
+    // Add products to basket
+    $scope.addToCart = function(product) {
+      var found = false;
+      $scope.cart.products.forEach(function(item) {
+        if (item.id === product.id) {
+          item.quantity++;
+          found = true;
+        }
+      });
+      if (!found) {
+        $scope.cart.products.push(angular.extend({ quantity: 1 }, product));
+      }
+    };
+
+    // Calculate total price for products in Cart
+    $scope.getCartPrice = function() {
+      var total = 0;
+      $scope.cart.products.forEach(function(product) {
+        total += product.price * product.quantity;
+      });
+      return total;
+    };
+
+    // Calculate total number of items in Cart
+    $scope.getCartTotals = function() {
+      var totalInCart = 0;
+
+      $scope.cart.products.forEach(function(product) {
+        totalInCart = parseInt(totalInCart) + product.quantity;
+      });
+      return totalInCart;
+    };
+
+    // Cart Checkout
+    $scope.cartCheckOut = function() {
+      var checkout = $scope.cart;
+      window.sessionStorage.checkout = angular.toJson(checkout);
+      $state.go('checkout');
+    };
+  });
+
+  app.controller('CartController', function($scope, $state) {
+    var cartItems = angular.fromJson(window.sessionStorage.checkout || '[]');
+    $scope.cart = cartItems;
 
 
-        // Add products to basket
-        $scope.addToCart = function(product) {
-            var found = false;
-            $scope.cart.products.forEach(function(item) {
-                if (item.id === product.id) {
-                    item.quantity++;
-                    found = true;
-                }
-            });
-            if (!found) {
-                $scope.cart.products.push(angular.extend({ quantity: 1 }, product));
-            }
-        };
+    // Add products to basket
+    $scope.addToCart = function(product) {
+      var found = false;
+      $scope.cart.products.forEach(function(item) {
+        if (item.id === product.id) {
+          item.quantity++;
+          found = true;
+        }
+      });
+      if (!found) {
+        $scope.cart.products.push(angular.extend({ quantity: 1 }, product));
+      }
+    };
 
-        //Remove product from basket
-        $scope.removeFromCart = function(product, index) {
-            var found = false;
-            $scope.cart.products.forEach(function(item) {
-                if (item.id === product.id) {
-                    item.quantity--;
-                    found = true;
-                    if (item.quantity === 0) {
-                        $scope.cart.products.splice(index, 1);
-                    }
-                }
-            });
-            if (!found) {
-                $scope.cart.products.splice(angular.extend({ quantity: -1 }, product));
-            }
-        };
+    //Remove product from basket
+    $scope.removeFromCart = function(product, index) {
+      var found = false;
+      $scope.cart.products.forEach(function(item) {
+        if (item.id === product.id) {
+          item.quantity--;
+          found = true;
+          if (item.quantity === 0) {
+            $scope.cart.products.splice(index, 1);
+          }
+        }
+      });
+      if (!found) {
+        $scope.cart.products.splice(angular.extend({ quantity: -1 }, product));
+      }
+    };
 
-        // Calculate total price for products in Cart
-        $scope.getCartPrice = function() {
-            var total = 0;
-            $scope.cart.products.forEach(function(product) {
-                total += product.price * product.quantity;
-            });
-            return total;
-        };
+    // Calculate total price for products in Cart
+    $scope.getCartPrice = function() {
+      var total = 0;
+      $scope.cart.products.forEach(function(product) {
+        total += product.price * product.quantity;
+      });
+      return total;
+    };
 
-        // Calculate total number of items in Cart
-        $scope.getCartTotals = function() {
-            var totalInCart = 0;
+    // Calculate total number of items in Cart
+    $scope.getCartTotals = function() {
+      var totalInCart = 0;
 
-            $scope.cart.products.forEach(function(product) {
-                totalInCart = parseInt(totalInCart) + product.quantity;
-            });
-            return totalInCart;
-        };
+      $scope.cart.products.forEach(function(product) {
+        totalInCart = parseInt(totalInCart) + product.quantity;
+      });
+      return totalInCart;
+    };
 
-    });
+    // Sends order via email
+    $scope.sendOrder = function() {
+        var newOrder;
+
+    };
+  });
 
 }());
