0931e94a5ae3e3bcfcf5ff643d31ab239d854275
Euan_G <euan@amwic.co.za>
2016-04-15 08:59:28 +0200

    Added main route to Express server

--- server.js
+++ server.js
@@ -2,11 +2,15 @@
   'use strict';
 
   var express = require('express');
-  var server = express();
+  var app = express();
 
-  server.use(express.static('build'));
-  server.listen(3000, function() {
-    console.log('The express frontend server is running on port 3000!, ctrl-c to quit');
+  app.use(express.static('./build'));
+
+  var main = require('./routes/main');
+  app.use('/', main);
+
+  app.listen(3000, function() {
+    console.log('ExpressJS server is running on port 3000!, ctrl-c to quit');
   });
 
 }());
