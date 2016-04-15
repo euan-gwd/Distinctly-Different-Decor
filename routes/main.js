4557880a07f31c3fca1496775a89e5d4f90202d1
Euan_G <euan@amwic.co.za>
2016-04-15 11:05:50 +0200

    Added email functionality
    Added NodeMailer and SendGrid to email order form.

--- routes/main.js
+++ routes/main.js
@@ -3,14 +3,40 @@
 
   var express = require('express');
   var router = express.Router();
+
+  var dotenv = require('dotenv');
+  dotenv.load();
+  var sendgrid_username = process.env.SENDGRID_USERNAME;
+  var sendgrid_password = process.env.SENDGRID_PASSWORD;
+
   var nodemailer = require('nodemailer');
+  var sgTransport = require('nodemailer-sendgrid-transport');
+  var options = {
+    auth: {
+      api_user: sendgrid_username,
+      api_key: sendgrid_password
+    }
+  };
+  var mailer = nodemailer.createTransport(sgTransport(options));
 
   router.route('/').get(function(req, res) {
     res.sendFile('index.html');
   });
 
   router.route('/send').get(function(req, res) {
+    var email = {
+      to: 'info@amwic.co.za',
+      from: 'DDDWebAppTest@ddd.co.za',
+      subject: 'Test Send from new Web App',
+      html: '<h1>Hello, This is my first email through SendGrid</h1>'
+    };
 
+    mailer.sendMail(email, function(err, res) {
+      if (err) {
+        console.log(err);
+      }
+      console.log(res);
+    });
   });
 
   module.exports = router;
