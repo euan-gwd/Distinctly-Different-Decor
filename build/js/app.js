05e996d7a006f7f729b4959649cd8e322d780faa
Euan_G <euan@amwic.co.za>
2016-04-15 19:33:42 +0200

    Cleaned up and refactored Form Validation Styles

--- build/js/app.js
+++ build/js/app.js
@@ -1,34 +1,34 @@
 (function() {
-    "use strict";
-    var app = angular.module('DDDApp', ['ui.router','DDDApp.controllers']);
+  "use strict";
+  var app = angular.module('DDDApp', ['ui.router', 'DDDApp.controllers']);
 
-    app.config(function($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state("home", {
-                url: "",
-                templateUrl: "js/views/home.html",
-                controller: "StoreController"
-            })
-            .state("checkout", {
-                url: "/checkout",
-                templateUrl: "js/views/checkout.html",
-                controller: "CartController"
-            });
-        $urlRouterProvider.otherwise('');
-    });
+  app.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state("home", {
+        url: "",
+        templateUrl: "js/views/home.html",
+        controller: "StoreController"
+      })
+      .state("checkout", {
+        url: "/checkout",
+        templateUrl: "js/views/checkout.html",
+        controller: "CartController"
+      });
+    $urlRouterProvider.otherwise('');
+  });
 
-    app.service('dataService', function($http) {
-        var currentCart = [];
-        this.getProducts = function(callback) {
-            $http.get('products.json').then(callback);
-        };
-    });
+  app.service('dataService', function($http) {
+    var currentCart = [];
+    this.getProducts = function(callback) {
+      $http.get('products.json').then(callback);
+    };
+  });
 
-    app.service('emailService', function($http) {
-        var currentCart = [];
-        this.getProducts = function(callback) {
-            $http.get('products.json').then(callback);
-        };
-    });
+  app.service('emailService', function($http) {
+    var currentCart = [];
+    this.getProducts = function(callback) {
+      $http.get('products.json').then(callback);
+    };
+  });
 
 }());
