161f9a98a3f9c156c3ddeecd43eae85249138185
Euan_G <euan@amwic.co.za>
2016-04-18 14:57:43 +0200

    Completed send to email route
    Finally completed the send to email of the order.

--- routes/main.js
+++ routes/main.js
@@ -5,58 +5,50 @@
   var router = express.Router();
   var dotenv = require('dotenv');
   dotenv.load();
-
-  // var sendgrid_username = process.env.SENDGRID_USERNAME;
-  // var sendgrid_password = process.env.SENDGRID_PASSWORD;
   var sendgrid_api_key = process.env.SENDGRID_APIKEY;
-
-  // var sendgrid = require('sendgrid')(sendgrid_username, sendgrid_password);
   var sendgrid = require('sendgrid')(sendgrid_api_key);
-
-
-  // var nodemailer = require('nodemailer');
-  // var sgTransport = require('nodemailer-sendgrid-transport');
-  // var options = {
-  //   auth: {
-  //     api_user: sendgrid_username,
-  //     api_key: sendgrid_password
-  //   }
-  // };
-  // var mailer = nodemailer.createTransport(sgTransport(options));
 
   // Main Client Route 
   router.route('/').get(function(req, res) {
     res.sendFile('index.html');
   });
 
-  // Send Mail Api Route
+  // Send Mail Route
   router.route('/checkout/sendOrder').post(function(req, res) {
     var data = req.body;
-    console.log(data.customerOrder[0].title);
+
     var email = new sendgrid.Email();
 
-    email.addTo = 'info@amwic.co.za';
-    email.setFrom = 'DDDWebAppTest@ddd.co.za';
-    email.subject = 'Test Order from ' + data.customerName;
-    email.html = '<h1> New Order from Website</h1><p>Name:'+ data.customerName + '</p><p>Phone:'+ data.customerPhone + '</p><p>Email:'+ data.customerEmail +'</p><p>Delivery Address:' + data.customerDelAddr +'</p><p>Order Details:'+ data.customerOrder +'</p>';
+    // Email message content and settings
+    function Message() {
+      var orderDetails = '<ul>';
+      for (var i = 0; i < data.customerOrder.length; i += 1) {
+        orderDetails += '<li>';
+        orderDetails += '<p> item: ' + data.customerOrder[i].title + '</p>';
+        orderDetails += '<p> qty: ' + data.customerOrder[i].quantity + '</p>';
+        orderDetails += '<p> price: ' + data.customerOrder[i].price + '</p>';
+        orderDetails += '</li>';
+      }
+      orderDetails += '</ul>';
+      return orderDetails;
+    }
 
-    // console.log(email);
-    // res.json(data);
+    email.to = 'info@amwic.co.za';
+    email.from = 'DDDWebAppTest@ddd.co.za';
+    email.setFromName = 'DDD Web App';
+    email.subject = 'Test Order from ' + data.customerName;
+    email.html = '<p>Name: ' + data.customerName + '</p>' + '<p>Phone: ' + data.customerPhone + '</p>' + '<p>Email: ' + data.customerEmail + '</p>' + '<p>Delivery Address: ' + data.customerDelAddr + '</p>' + '<p>Order Details: ' + Message() + '</p>';
 
     // SendGrid Mailer
-    // sendgrid.send(email, function(err, json) {
-    //   if (err) {
-    //     return console.error(err);
-    //   }
-    //   console.log(json);
-    // });
+    sendgrid.send(email, function(err, json) {
+      if (err) {
+        return console.error(err);
+      }
+      console.log(json);
+    });
 
-    // mailer.sendMail(email, function(err, res) {
-    //   if (err) {
-    //     console.log(err);
-    //   }
-    //   console.log(res);
-    // });
+    res.json(data);
+
   });
 
   module.exports = router;
