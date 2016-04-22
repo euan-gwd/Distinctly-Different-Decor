4a9ad539fdde655b90e21240a8190bffdad9a142
Euan_G <euan@amwic.co.za>
2016-04-22 09:48:18 +0200

    Completed Layouts and changed location of data files
    Completed Layouts and changed location of data files, added extra
    styles to MDL icon fonts css

--- build/js/services.js
+++ build/js/services.js
@@ -5,15 +5,8 @@
     app.service('dataService', function($http) {
         var currentCart = [];
         this.getProducts = function(callback) {
-            $http.get('products.json').then(callback);
+            $http.get('js/data/products.json').then(callback);
         };
     });
-
-    // app.service('emailService', function($http) {
-    //     var currentCart = [];
-    //     this.sendOrder = function(callback) {
-    //         $http.post(data).then(callback);
-    //     };
-    // });
 
 }());
