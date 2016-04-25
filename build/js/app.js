79497b8c8ab92e2cf4c9328591d4d1e7560dbed1
Euan_G <euan@amwic.co.za>
2016-04-25 15:34:42 +0200

    Added theme and toolbar

--- build/js/app.js
+++ build/js/app.js
@@ -1,25 +1,32 @@
 (function() {
-  "use strict";
-  var app = angular.module('DDDApp', ['ui.router', 'DDDApp.controllers', 'DDDApp.services','ngMaterial']);
+    "use strict";
+    var app = angular.module('DDDApp', ['ui.router', 'DDDApp.controllers', 'DDDApp.services', 'ngMaterial']);
 
-  app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-    $stateProvider
-      .state("home", {
-        url: "/",
-        templateUrl: "js/views/home.html",
-        controller: "StoreController"
-      })
-      .state("checkout", {
-        url: "/checkout",
-        templateUrl: "js/views/checkout.html",
-        controller: "CartController"
-      })
-      .state("thankyou", {
-        url: "/thankyou",
-        templateUrl: "js/views/thankyou.html"
-      });
-    $urlRouterProvider.otherwise('/');
-    $locationProvider.html5Mode(false);
-  });
+    app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $mdThemingProvider, $mdIconProvider) {
+        $stateProvider
+            .state("home", {
+                url: "/",
+                templateUrl: "js/views/home.html",
+                controller: "StoreController"
+            })
+            .state("checkout", {
+                url: "/checkout",
+                templateUrl: "js/views/checkout.html",
+                controller: "CartController"
+            })
+            .state("thankyou", {
+                url: "/thankyou",
+                templateUrl: "js/views/thankyou.html"
+            });
+        $urlRouterProvider.otherwise('/');
+        $locationProvider.html5Mode(false);
+
+        $mdThemingProvider.theme('default')
+            .primaryPalette('orange')
+            .accentPalette('deep-purple')
+            .warnPalette('red');
+
+        $mdIconProvider.defaultFontSet('material-icons');   
+    });
 
 }());
