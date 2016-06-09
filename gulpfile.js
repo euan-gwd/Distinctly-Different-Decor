6170c821c5001d0412c222d093a01a30e7a50ee8
Euan_G <euan@amwic.co.za>
2016-06-09 16:28:36 +0200

    Refactored to use best practices for express server

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,11 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     reload = browserSync.reload,
     plumber = require('gulp-plumber'),
-    flatten = require('gulp-flatten'),
-    del = require('del'),
-    runSequence = require('run-sequence'),
     nodemon = require('gulp-nodemon');
-
-// -----------------
-// Development Enviroment
-// -----------------
 
 // Compile Sass into CSS
 gulp.task('sass', function () {
-    return gulp.src('build/scss/**/*.scss') // Gets all files ending with .scss in build/scss
+    return gulp.src('public/scss/**/*.scss') // Gets all files ending with .scss in public/scss
         .pipe(plumber({
             handleError: function (err) {
                 console.log(err);
@@ -24,7 +17,7 @@ gulp.task('sass', function () {
         }))
         .pipe(sass()) // Converts Sass to CSS with gulp-sass
         .pipe(prefix("last 2 versions", "> 1%", "ie 8", "Android 2", "Firefox ESR")) //adds vendor prefixes if needed
-        .pipe(gulp.dest('build/css')) // outputs CSS to build/css
+        .pipe(gulp.dest('public/css')) // outputs CSS to public/css
         .pipe(reload({
             stream: true
         }));
@@ -32,14 +25,14 @@ gulp.task('sass', function () {
 
 // checks for errors in JS files
 gulp.task('scripts', function () {
-    return gulp.src('build/js/**.*.js')
+    return gulp.src('public/js/**.*.js')
         .pipe(plumber({
             handleError: function (err) {
                 console.log(err);
                 this.emit('end');
             }
         }))
-        .pipe(gulp.dest('build/js'))
+        .pipe(gulp.dest('public/js'))
         .pipe(reload({
             stream: true
         }));
@@ -63,86 +56,15 @@ gulp.task('nodemon', function (cb) {
 gulp.task('browserSync', ['nodemon'], function () {
     browserSync.init(null, {
         proxy: "http://localhost:3000",
-        files: ["build/**/*.*"],
+        files: ["public/**/*.*"],
         ui: false,
         port: 4000,
     });
 });
 
 // Watches for file changes and reloads browsers
-gulp.task('serve', ['sass', 'scripts', 'browserSync'], function () {
-    gulp.watch('build/scss/**/*.scss', ['sass']);
-    gulp.watch('build/js/**/*.js', ['scripts']);
-    gulp.watch('build/**/*.html', reload);
-});
-
-// -----------------
-// Production Enviroment
-// -----------------
-
-//Cleans out the dist folder
-gulp.task('dist-clean', function () {
-    return del('dist/**/*');
-});
-
-// Compiles Sass into CSS then outputs it to the dist folder
-gulp.task('build-sass', function () {
-    return gulp.src('build/scss/**/*.scss') // Gets all files ending with .scss in build/scss
-        .pipe(plumber({
-            handleError: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        }))
-        .pipe(sass()) // Converts Sass to CSS with gulp-sass
-        .pipe(prefix("last 2 versions", "> 1%", "ie 8", "Android 2", "Firefox ESR")) //adds vendor prefixes if needed
-        .pipe(gulp.dest('dist/css')); //outputs it to the dist folder
-});
-
-// copies from bower components in build folder and compiles it to the dist folder
-gulp.task('build-bower', function () {
-    gulp.src(['build/bower_components/**/angular-*.min.js','!build/bower_components/angular-material/modules/**/*.min.js'])
-        .pipe(flatten())
-        .pipe(gulp.dest('dist/js/angular'));
-});
-
-// Copies fonts to the dist folder
-gulp.task('copy-fonts', function () {
-    return gulp.src('build/fonts/**/*', {
-            base: 'build'
-        })
-        .pipe(plumber({
-            handleError: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        }))
-        .pipe(gulp.dest('dist'));
-});
-
-// checks for errors in JS files then compiles it to the dist folder
-gulp.task('build-scripts', function () {
-    return gulp.src('build/js/**/*', {
-            base: 'build'
-        })
-        .pipe(plumber({
-            handleError: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        }))
-        .pipe(gulp.dest('dist'));
-});
-
-// Copies html to the dist folder
-gulp.task('build-html', function () {
-    return gulp.src('build/index.html')
-        .pipe(gulp.dest('dist/'));
-});
-
-// Builds Production App in dist folder
-gulp.task('build', function (cb) {
-    runSequence('dist-clean', ['build-sass', 'build-scripts'],
-        'copy-fonts', 'build-html',
-        cb);
+gulp.task('default', ['sass', 'scripts', 'browserSync'], function () {
+    gulp.watch('public/scss/**/*.scss', ['sass']);
+    gulp.watch('public/js/**/*.js', ['scripts']);
+    gulp.watch('public/**/*.html', reload);
 });
--- server.js
+++ server.js
@@ -4,13 +4,13 @@
   var express    = require('express'),
       app        = express(),
       bodyParser = require('body-parser'),
-			main 			 = require(__dirname +'/build/routes/main'),
+			main 			 = require(__dirname +'/routes/main'),
       port       = 3000;
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.use(express.static(__dirname +'/build'));
+  app.use(express.static(__dirname +'/public'));
 
   app.use('/', main);
 
