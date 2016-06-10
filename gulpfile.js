439166bb29044c346768b0536c3986d4bfc46e97
Euan_G <euan@amwic.co.za>
2016-06-10 14:33:07 +0200

    cleaned up and refactored gulp-sass task

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,9 @@ var gulp = require('gulp'),
     nodemon = require('gulp-nodemon');
 
 // Compile Sass into CSS
-gulp.task('sass', function () {
+gulp.task('styles', function () {
     return gulp.src('public/scss/**/*.scss') // Gets all files ending with .scss in public/scss
-        .pipe(plumber({
-            handleError: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        }))
-        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+        .pipe(sass().on('error', sass.logError)) // Converts Sass to CSS with gulp-sass
         .pipe(prefix("last 2 versions", "> 1%", "ie 8", "Android 2", "Firefox ESR")) //adds vendor prefixes if needed
         .pipe(gulp.dest('public/css')) // outputs CSS to public/css
         .pipe(reload({
@@ -63,8 +57,8 @@ gulp.task('browserSync', ['nodemon'], function () {
 });
 
 // Watches for file changes and reloads browsers
-gulp.task('default', ['sass', 'scripts', 'browserSync'], function () {
-    gulp.watch('public/scss/**/*.scss', ['sass']);
+gulp.task('default', ['styles', 'scripts', 'browserSync'], function () {
+    gulp.watch('public/scss/**/*.scss', ['styles']);
     gulp.watch('public/js/**/*.js', ['scripts']);
     gulp.watch('public/**/*.html', reload);
 });
